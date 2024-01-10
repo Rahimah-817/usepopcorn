fc45346f725df7451c3457d64313591aec7145b5
Rahimah Ansari <rahimeansari7@gmail.com>
2024-01-10 21:35:01 +0430

    Using an async Function

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ const KEY = 'f84fc31d';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = 'interstellar';
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
