927413ab2a6f483d1a46a079e73220f3b5aa2b4c
Rahimah Ansari <rahimeansari7@gmail.com>
2024-02-10 14:38:34 +0430

    One More Effect Listening to a Keypress

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const average = (arr) =>
 const KEY = 'f84fc31d';
 
 export default function App() {
-  const [query, setQuery] = useState('inception');
+  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -48,8 +48,8 @@ export default function App() {
           setMovies(data.Search);
           setError('');
         } catch (err) {
-          console.error(err.message);
           if (err.name !== 'AbortError') {
+            console.log(err.message);
             setError(err.message);
           }
         } finally {
@@ -62,7 +62,7 @@ export default function App() {
         setError('');
         return;
       }
-
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -262,6 +262,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === 'Escape') {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
